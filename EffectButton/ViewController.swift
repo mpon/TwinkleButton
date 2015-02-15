961aa759fcbbb7a470ddf9566bf0aeb8058ce1d5
Masato Oshima <qqw75pn9@gmail.com>
2015-02-15 21:35:13 +0900

    Initial Commit

---
+++ EffectButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EffectButton
+//
+//  Created by Masato Oshima on 2015/02/15.
+//  Copyright (c) 2015年 Masato Oshima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
